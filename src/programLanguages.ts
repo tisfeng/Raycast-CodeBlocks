14917ca105cb619d837b97b6549e05b55e761b3c
tisfeng <tisfeng@gmail.com>
2022-07-10 00:41:13 +0800

    perf: add more program languages

--- src/programLanguages.ts
+++ src/programLanguages.ts
@@ -2,7 +2,7 @@
  * @author: tisfeng
  * @createTime: 2022-07-07 11:42
  * @lastEditor: tisfeng
- * @lastEditTime: 2022-07-07 16:02
+ * @lastEditTime: 2022-07-10 00:40
  * @fileName: programLanguages.ts
  *
  * Copyright (c) 2022 by tisfeng, All Rights Reserved.
@@ -15,13 +15,25 @@ export interface ProgramLanguage {
 
 /**
  * programLanguages, include Objective-C, Swift, JavaScript, TypeScript, Bash, C, Python
+ *
+ * ref: https://github.com/github/linguist/blob/master/lib/linguist/languages.yml
  */
 export const programLanguages: ProgramLanguage[] = [
   { name: "Objective-C", value: "objc" },
   { name: "Swift", value: "swift" },
   { name: "JavaScript", value: "js" },
   { name: "TypeScript", value: "ts" },
+  { name: "TSX", value: "tsx" },
   { name: "Bash", value: "bash" },
   { name: "C", value: "c" },
   { name: "Python", value: "py" },
+  { name: "C++", value: "cpp" },
+  { name: "C#", value: "cs" },
+  { name: "Go", value: "go" },
+  { name: "Rust", value: "rs" },
+  { name: "Ruby", value: "rb" },
+  { name: "PHP", value: "php" },
+  { name: "Perl", value: "pl" },
+  { name: "R", value: "r" },
+  { name: "SQL", value: "sql" },
 ];
